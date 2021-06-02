5405e4721ccefc02ce1cd64ac33f645e6744584f
JuvenalDev <cientistjuvenal@gmail.com>
2021-06-02 11:16:48 +0100

    Initial Commit

---
+++ New Apostolic Church/New_Apostolic_ChurchApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_Apostolic_ChurchApp.swift
+//  New Apostolic Church
+//
+//  Created by Juvenal Lunguenda on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct New_Apostolic_ChurchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
