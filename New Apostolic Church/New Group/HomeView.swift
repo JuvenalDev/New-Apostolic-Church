a0961595f2e343682d80a26b708f0f5a1ef348d4
JuvenalDev <cientistjuvenal@gmail.com>
2021-06-02 17:41:21 +0100

    Initial Commitment HomeView Design Word Of Month Design

---
+++ New Apostolic Church/New Group/HomeView.swift
@@ -0,0 +1,125 @@
+//
+//  HomeView.swift
+//  New Apostolic Church
+//
+//  Created by Juvenal Lunguenda on 6/2/21.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+
+    @State var search = ""
+    @State var activePressedColor = false
+    @State var selectedMenu:String? = ""
+
+    var body: some View {
+        NavigationView {
+            VStack{
+
+                VStack {
+                    //Search Section
+                    HStack(spacing: 15) {
+
+                        TextField("Procurar Comunidades", text : $search)
+
+                        Button(action: {
+
+                        }, label: {
+
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(.gray)
+
+                        })
+
+                    }
+                    .padding(.horizontal)
+                    .padding(.vertical,10)
+                    .background(Color.black.opacity(0.06))
+                    .clipShape(Capsule())
+                    .padding(.horizontal)
+
+                    Image("inaLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(.leading, 60)
+                        .padding(.trailing,60)
+                        .padding()
+
+                }
+                Divider()
+                    .background(Color.blue)
+                    .padding()
+                    .shadow(radius: 100)
+
+                //MARK: - Custom Menu
+                HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 20) {
+
+                    NavigationLink(
+                        destination: WordOfTheMonthView(),
+                        tag: "wOfMonth",
+                        selection: self.$selectedMenu)
+                    {
+
+                        VStack{
+                            Text("Palavra do Mês ")
+                                .bold()
+                                .padding(.bottom, 5)
+                            Image(systemName: "book")
+
+                        }
+                        .foregroundColor(selectedMenu == "wOfMonth" ?  Color.black : Color.white)
+                    }
+                    .background(selectedMenu == "wOfMonth" ?  Color.white : Color.black)
+                    .cornerRadius(10)
+
+                    NavigationLink(
+                        destination: Text("Hinário ") ,
+                        tag: "musicBook",
+                        selection: self.$selectedMenu)
+                    {
+                        VStack{
+                            Text("Hinário ")
+                                .bold()
+                                .padding(.bottom, 5)
+                            Image(systemName: "music.quarternote.3")
+                        }
+                        .foregroundColor(selectedMenu == "musicBook" ?  Color.black : Color.white)
+
+                    }
+                    .background(selectedMenu == "musicBook" ?  Color.white : Color.black)
+                    .cornerRadius(10)
+
+                    NavigationLink(
+                        destination: Text("Comunicados"),
+                        tag: "announcements",
+                        selection: self.$selectedMenu)
+                    {
+                        VStack{
+                            Text("Comunicados")
+                                .bold()
+                                .padding(.bottom, 5)
+                            Image(systemName: "paperplane")
+                        }
+                        .foregroundColor(selectedMenu == "announcements" ?  Color.black : Color.white)
+
+                    }
+                    .background(selectedMenu == "announcements" ?  Color.white : Color.black)
+                    .cornerRadius(10)
+
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+                .cornerRadius(20, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ New Apostolic Church/New Group/WordOfTheMonthView.swift
@@ -0,0 +1,46 @@
+//
+//  WordOfTheMonthView.swift
+//  New Apostolic Church
+//
+//  Created by Juvenal Lunguenda on 6/2/21.
+//
+
+import SwiftUI
+
+struct WordOfTheMonthView: View {
+
+    @State var wordOfMonthTitle = "Seja feita a Tua vontade"
+    @State var wordOfMonth = "Quando se fala da vontade de Deus, frequentemente, é num contexto negativo. \n \n Se sofremos uma desgraça: Foi a vontade de Deus. Se uma oração não é ouvida: O que orámos não está de acordo com a vontade de Deus.\n \n No Pai Nosso, a afirmação „Seja feita a Tua vontade“ significa, para muitos, submeter-se à vontade de Deus e, para isso, prescindir de algo. Curvamo-nos perante a vontade de Deus – mas, na verdade, preferíamos que fosse diferente."
+
+    var body: some View {
+        NavigationView {
+            VStack{
+            Text(wordOfMonthTitle )
+                .bold()
+                .padding()
+                .font(.title)
+
+                Divider()
+                    .padding()
+
+                Text(wordOfMonth)
+                    .foregroundColor(.black)
+                    .bold()
+                    .padding(10)
+                    //.background(Color.gray)
+                    //.opacity(0.8)
+                    .cornerRadius(20, antialiased: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    .padding(10)
+
+            }
+            .navigationBarTitle(Text("Palavra do Mês"))
+
+        }
+    }
+}
+
+struct WordOfTheMonthView_Previews: PreviewProvider {
+    static var previews: some View {
+        WordOfTheMonthView()
+    }
+}
